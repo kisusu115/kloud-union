48a983edab2801bd2e82ec013857e5de0f05b246
kisusu <kikisu115@naver.com>
2024-10-13 03:46:48 +0900

    feat: user의 station 관련 info 수정 api 추가
    session에서 user 정보를 가져오기 위해서는 authenticateSession를 꼭 미들웨어로 설정 해주어야 함

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getUsers, getUserProfile, updateUserProfile, redirectToKakao } = require('../controllers/userController');
+const { getUsers, getUserProfile, updateUserProfile, updateUserStation, redirectToKakao } = require('../controllers/userController');
 const { authenticateSession } = require('../middlewares/authMiddleware');
 const router = express.Router();
 
@@ -173,9 +173,90 @@ router.get('/profile', authenticateSession, getUserProfile);
  *                   type: string
  *                   example: 'Server error'
  */
-router.post('/profile', updateUserProfile);
+router.post('/profile', authenticateSession, updateUserProfile);
 
-// 회원 탈퇴 (주석 처리된 부분)
-// router.delete('/profile', getUserInfo);
+/**
+ * @swagger
+ * /api/user/station:
+ *   post:
+ *     summary: 사용자의 지하철 역 정보 수정
+ *     tags: [User]
+ *     description: 세션의 snsId를 통해 사용자의 지하철 관련 정보를 업데이트합니다. 역 이름, 노선 번호, 상하행 정보를 수정할 수 있습니다.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               stationName:
+ *                 type: string
+ *                 description: 사용자가 선택한 지하철 역 이름
+ *                 example: "건대입구"
+ *               line:
+ *                 type: number
+ *                 description: 사용자가 선택한 지하철 노선 번호
+ *                 example: 2
+ *               upDown:
+ *                 type: number
+ *                 description: 상행선(1) 또는 하행선(2)
+ *                 example: 1
+ *     responses:
+ *       200:
+ *         description: 성공적으로 사용자 지하철 정보가 수정되었습니다.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *             examples:
+ *               user:
+ *                 value: {
+ *                   "_id": "670a82d3149a8c0f5689f57f",
+ *                   "snsId": "3739459145",
+ *                   "nickname": "제형",
+ *                   "weight": 1,
+ *                   "age": 24,
+ *                   "gender": "Male",
+ *                   "height": 170,
+ *                   "stationName": "건대입구",
+ *                   "line": 2,
+ *                   "upDown": 1,
+ *                   "createdAt": "2024-10-12T14:08:19.219Z",
+ *                   "updatedAt": "2024-10-12T14:08:19.219Z",
+ *                   "__v": 0
+ *                 }
+ *       400:
+ *         description: 요청에 필요한 데이터가 포함되지 않았습니다.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   example: 'Required fields are missing'
+ *       404:
+ *         description: 해당 사용자 정보를 찾을 수 없습니다.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   example: 'User not found'
+ *       500:
+ *         description: 서버 오류 발생.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   example: 'Server error'
+ */
+router.post('/station', authenticateSession, updateUserStation);
+
 
 module.exports = router;
