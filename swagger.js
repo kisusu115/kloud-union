ef96744e8c995419e253478d564777033dfb7509
Ubuntu <ubuntu@ip-172-31-7-223.ap-northeast-2.compute.internal>
2024-10-15 12:43:42 +0000

    fix: swagger baseurl 문제로 ec2에선 실행 안돼서 수정

--- swagger.js
+++ swagger.js
@@ -9,7 +9,7 @@ const options = {
         },
         servers: [
             {
-                url: 'http://127.0.0.1:3000/api', // API 서버 URL
+                url: '/', // API 서버 URL
             },
         ],
     },
